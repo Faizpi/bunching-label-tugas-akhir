d2d500f69b3ac9ee40708aacd991767048f8efa9
Faizpi <faizbintang1244@gmail.com>
2025-09-19 08:52:34 +0700

    fix : export data label

--- resources/views/web/label/print.blade.php
+++ resources/views/web/label/print.blade.php
@@ -82,22 +82,34 @@
                 <th>Formatted Lot Number</th>
                 <th>Type/Size</th>
                 <th>Length (m)</th>
+                <th>Total Length (m)</th>
                 <th>Extra Length (m)</th>
                 <th>Weight (kg)</th>
                 <!-- <th>Extra Weight (kg)</th> -->
-                <th>Shift Date</th>
+                <th>Date</th>
                 <th>Shift</th>
                 <th>Machine Number</th>
                 <th>Pitch (mm)</th>
                 <th>Visual</th>
-                <th>QC Test</th>
+                <!-- <th>QC Test</th> -->
                 <th>Remark</th>
                 <th>Operator Name</th>
-                <th>Created At</th>
+                <!-- <th>Created At</th> -->
+                <th>Print Count</th>
             </tr>
         </thead>
         <tbody>
             @forelse($labels as $label)
+                @php
+                    $lengthTotal = null;
+                    if (!empty($label->length)) {
+                        // contoh format: "2 x 1800" atau "2x1800"
+                        $parts = preg_split('/x/i', str_replace(' ', '', $label->length));
+                        if (count($parts) == 2 && is_numeric($parts[0]) && is_numeric($parts[1])) {
+                            $lengthTotal = $parts[0] * $parts[1];
+                        }
+                    }
+                @endphp
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{ $label->id }}</td>
@@ -105,20 +117,22 @@
                     <td>{{ $label->formated_lot_number }}</td>
                     <td>{{ $label->type_size }}</td>
                     <td>{{ $label->length }}</td>
+                    <td>{{ $lengthTotal }}</td>
                     <td>{{ $label->extra_length }}</td>
                     <td>{{ $label->weight }}</td>
                     <!-- <td>{{ $label->extra_weight }}</td> -->
                     <!-- <td>{{ $label->shift_date }}</td> -->
-                    <td>{{ \Carbon\Carbon::parse($label->shift_date)->format('d-m-Y') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($label->shift_date)->format('d-M-Y') }}</td>
                     <td>{{ $label->shift }}</td>
                     <td>{{ $label->machine_number }}</td>
                     <td>{{ $label->pitch }}</td>
                     <td>{{ $label->visual }}</td>
-                    <td>{{ $label->bobin_no }}</td>
+                    <!-- <td>{{ $label->bobin_no }}</td> -->
                     <td>{{ $label->remark }}</td>
                     <td>{{ $label->operator->name ?? '-' }}</td>
                     <!-- <td>{{ $label->created_at }}</td> -->
-                    <td>{{ \Carbon\Carbon::parse($label->created_at)->format('d-m-Y H:i') }}</td>
+                    <!-- <td>{{ \Carbon\Carbon::parse($label->created_at)->format('d-m-Y H:i') }}</td> -->
+                    <td>{{ $label->print_count }}</td>
                 </tr>
             @empty
                 <tr>
@@ -130,7 +144,7 @@
 
     {{-- Footer --}}
     <div class="footer">
-        Dicetak pada: {{ now()->format('d-m-Y H:i') }} <br>
+        Dicetak pada: {{ now()->format('d-M-Y H:i') }} <br>
         Oleh: {{ auth()->user()->name ?? 'System' }}
     </div>
 
